30d5cc74f4cfea39ed331e0f1d3240db09eef56e
dcancho <d.cancho.c@gmail.com>
2022-04-26 11:02:40 -0500

    test1

--- TA1/TA1.cpp
+++ TA1/TA1.cpp
@@ -1,8 +1,11 @@
 #include "pch.h"
+#include <iostream>
+#include <conio.h>
 
 using namespace System;
 
 int main(array<System::String ^> ^args)
 {
+    std::cout << "Hello world!";
     return 0;
 }
