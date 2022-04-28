66e857f2a331928aadfbdd7d91730f41e7fbabdb
dcancho <d.cancho.c@gmail.com>
2022-04-27 20:44:02 -0500

    test4

--- TA1/Meteorito.h
+++ TA1/Meteorito.h
@@ -3,15 +3,22 @@
 class Meteorito
 {
 private:
-	int ypos, xpos, vel;
+	int ypos, xpos;
+	float velocity;
 public:
 	Meteorito()
 	{
 		System::Random r;
 		ypos = r.Next(0, 81);
 		xpos = r.Next(0, 41);
-		vel = r.Next(0; 100) / 100;
+		velocity = r.Next(0, 100) / 100;
+
 	}
 	~Meteorito(){}
-
+	Meteorito(int x, int y, float vel)
+	{
+		ypos = y;
+		xpos = x;
+		velocity = vel;
+	}
 };
