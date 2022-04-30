cf068ebaf0d7ff0b592d7e0af11ed2f60aa80974
Jose Anthony Arenas Conde <u20211d744@upc.edu.pe>
2022-04-30 15:39:51 -0500

    Setters

--- TA1/Cliente.h
+++ TA1/Cliente.h
@@ -15,7 +15,18 @@ public:
 	}
 	//Destructor
 	~Cliente() {}
-	
+	//getters
+	short getposX_Cliente() { return posX; }
+	short getposY_Cliente() { return posY; }
+	short getdX_Cliente() { return dX; }
+	short getdY_Cliente() { return dY; }
+	short isVIP() { return isVIP; }
+	//setters
+	void setposX_Cliente(short v) { posX = v; }
+	void setposY_Cliente(short v) { posY = v; }
+	void setdX_Cliente(short v) { dX = v; }
+	void setdY_Cliente(short v) { dY = v; }
+	void setisVIP(bool v) {isVIP = v; }
 
 
 
