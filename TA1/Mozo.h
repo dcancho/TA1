f289caaf43b2495ff6a9c12b8ddfaa995b348bd2
Jose Anthony Arenas Conde <u20211d744@upc.edu.pe>
2022-04-30 19:07:32 -0500

    sasa

--- TA1/Mozo.h
+++ TA1/Mozo.h
@@ -11,6 +11,8 @@ public:
 	Mozo(short posX, short posY, short v, short dX, short dY, short score, char caracter) :Entidad(posX, posY, v, dX, dY) {
 		this->score = score;
 		caracteres = caracter;
+		posX= 5 - rand() % (75 + 1 - 5);
+		posY= 5 - rand() % (35 + 1 - 5);
 	}
 	//Destructor
 	~Mozo() {}
@@ -30,6 +32,28 @@ public:
 	void Borrar() {
 		Console::SetCursorPosition(posX, posY);
 		cout << "   ";
+	}
+	void Dibujar() {
+		int opc;
+		opc = 1 - rand() % (6 + 1 - 1);
+		switch (opc)
+		{
+		case 1: Console::ForegroundColor = ConsoleColor::Red;
+			;break;
+		case 2:Console::ForegroundColor = ConsoleColor::Yellow;
+			;break;
+		case 3:Console::ForegroundColor = ConsoleColor::Green;
+			;break;
+		case 4:Console::ForegroundColor = ConsoleColor::DarkBlue;
+			;break;
+		case 5:Console::ForegroundColor = ConsoleColor::Gray;
+			;break;
+		case 6:Console::ForegroundColor = ConsoleColor::DarkCyan;
+			;break;
+
+		}
+		Console::SetCursorPosition(posX, posY); cout << "/_\";
+		Console::ForegroundColor = ConsoleColor::White;
 	}
 
 
